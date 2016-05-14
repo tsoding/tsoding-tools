a9f8a65f899fd7560a15173c6a4169c6ef6784e2
rexim <reximkut@gmail.com>
2016-05-14 22:17:50 +0600

    Rename Rest API URL variable

--- activity/current-activity-monitor.sh
+++ activity/current-activity-monitor.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-INPROGRESS_CARDS_URL="https://trello.com/1/list/56acc96e751324399cea0ce1/cards"
+REST_API_URL="https://trello.com/1/list/56acc96e751324399cea0ce1/cards"
 
 set -x
 
 while true; do
-    curl -s "$INPROGRESS_CARDS_URL" > in-progress.json
+    curl -s "$REST_API_URL" > in-progress.json
     echo -n "Current Activity: " > current-activity.txt
     if [ $(jq -r ".[0].name != null" in-progress.json) == "true" ]; then
         jq -r ".[0].name" in-progress.json >> current-activity.txt
