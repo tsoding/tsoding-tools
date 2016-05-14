27b7203f9b531a8fba0c6b8930ffe5fb09bb7cc5
rexim <reximkut@gmail.com>
2016-05-14 22:20:10 +0600

    Extract jq expression to a separate var

--- activity/current-activity-monitor.sh
+++ activity/current-activity-monitor.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 REST_API_URL="https://trello.com/1/list/56acc96e751324399cea0ce1/cards"
+JSON_JQ_EXTRACTOR=".[0].name"
 
 set -x
 
 while true; do
     curl -s "$REST_API_URL" > in-progress.json
     echo -n "Current Activity: " > current-activity.txt
-    if [ $(jq -r ".[0].name != null" in-progress.json) == "true" ]; then
-        jq -r ".[0].name" in-progress.json >> current-activity.txt
+    if [ $(jq -r "$JSON_JQ_EXTRACTOR != null" in-progress.json) == "true" ]; then
+        jq -r "$JSON_JQ_EXTRACTOR" in-progress.json >> current-activity.txt
     else
         echo "Farting" >> current-activity.txt
     fi
