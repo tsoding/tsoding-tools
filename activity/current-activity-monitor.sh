af8bb74aacabb06969864284b41202c9b1c4603d
rexim <reximkut@gmail.com>
2016-07-05 21:24:32 +0600

    Update midi-looper name in activity monitor

--- activity/current-activity-monitor.sh
+++ activity/current-activity-monitor.sh
@@ -2,8 +2,8 @@
 
 ## General parameters
 POLLING_INTERVAL=10
-# GITHUB_PROJECT="tsoding/midi-looper"
-GITHUB_PROJECT="rexim/Morganey"
+GITHUB_PROJECT="tsoding/dimooper"
+# GITHUB_PROJECT="rexim/Morganey"
 
 # # Trello parameters
 # REST_API_URL="https://trello.com/1/list/56acc96e751324399cea0ce1/cards"
