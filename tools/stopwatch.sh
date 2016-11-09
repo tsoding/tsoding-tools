1922e266e9f7326d306ffe6a23bc837692614e7c
rexim <reximkut@gmail.com>
2016-11-09 17:40:48 +0600

    Replace command with comment in stopwatch

--- tools/stopwatch.sh
+++ tools/stopwatch.sh
@@ -16,14 +16,6 @@ echo "${now}"
 
 while true; do 
     echo -n "> "
-    read command
-    case "${command}" in
-        "cut")
-            log "$(TZ=UTC date --date now-$now +%H:%M:%S.%N)"
-        ;;
-
-        *)
-            echo "'${command}' unrecognized command"
-        ;;
-    esac
+    read comment
+    log "\"${comment}\" $(TZ=UTC date --date now-$now +%s)"
 done
