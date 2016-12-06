a08930de9fae9da3c2072c6f303ecc17b9a9d8b2
rexim <reximkut@gmail.com>
2016-12-06 19:57:29 +0600

    Remove unused tools

--- tools/activity-trap.el
+++
@@ -1,9 +0,0 @@
-(defvar activity-trap-file-path "~/.activity-trap")
-
-(defun activity-trap (description)
-  (interactive "s")
-  (with-temp-buffer
-    (insert description)
-    (write-file activity-trap-file-path)))
-
-(global-set-key (kbd "C-c C-p") 'activity-trap)
