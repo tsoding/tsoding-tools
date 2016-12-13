30fafe31930cc2dd1632465a7f7808c8ab558208
rexim <reximkut@gmail.com>
2016-12-13 21:15:16 +0600

    Extract cut loop into a procedure

--- stopwatch/cut-by-stopwatch.py
+++ stopwatch/cut-by-stopwatch.py
@@ -1,6 +1,8 @@
 import csv
 import bpy
 
+fps = bpy.data.scenes['Scene'].render.fps / bpy.data.scenes['Scene'].render.fps_base
+
 def read_csv(csv_file_name):
     with open(csv_file_name, 'r') as csvfile:
         csvreader = csv.reader(csvfile)
@@ -10,9 +12,9 @@ def cut_at(frame):
     bpy.ops.sequencer.select_all(action='SELECT')
     bpy.ops.sequencer.cut(frame=frame, type='SOFT')
 
-fps = bpy.data.scenes['Scene'].render.fps / bpy.data.scenes['Scene'].render.fps_base
-stopwatch = "/home/rexim/MorningTsoding/Morganey/Ep.27/1480862482sec"
+def cut_by_stopwatch(stopwatch_filepath):
+    cut_at(0)
+    for [_, secs] in read_csv(stopwatch):
+        cut_at(secs * fps)
 
-cut_at(0)
-for [_, secs] in read_csv(stopwatch):
-    cut_at(secs * fps)
+cut_by_stopwatch("/home/rexim/MorningTsoding/Morganey/Ep.27/1480862482sec")
