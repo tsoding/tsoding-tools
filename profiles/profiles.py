b9fe364995ec432a2ccd708ad65628550a26b421
rexim <reximkut@gmail.com>
2017-01-16 03:52:22 +0600

    Introduce Youtube filter (#2)

--- profiles/profiles.py
+++ profiles/profiles.py
@@ -3,6 +3,7 @@
 import sys
 import yaml
 import codecs
+import re
 from os import path
 from jinja2 import Environment, PackageLoader
 
@@ -13,6 +14,10 @@ env = Environment(loader=PackageLoader(__name__, 'templates'))
 
 def usage():
     print 'Usage: profiles.py <recipe>'
+
+
+def youtube_filter(text):
+    return re.sub('\[(.*)\]\((.*)\)', '\\1: \\2', text)
 
 
 def read_file(file_path):
@@ -31,15 +36,25 @@ def get_project_params(project_name):
     return yaml.load(read_file(project_file_path))
 
 
-def render_profile(project_name, template_name):
-    profile_file_name = "%s-%s.md" % (project_name, template_name)
+def apply_filter(text, filter_name):
+    if filter_name == 'Youtube':
+        return youtube_filter(text)
+
+    return text
+
+
+def render_profile(project_name, template_name, filter_name):
+    profile_file_name = "%s-%s-%s.md" % (project_name,
+                                         template_name,
+                                         filter_name)
     profile_file_path = path.join(OUTPUT_PATH, profile_file_name)
     project_params = get_project_params(project_name)
     template = env.get_template('%s.jinja2' % (template_name))
 
     print "Rendering %s" % (profile_file_path)
 
-    write_file(profile_file_path, template.render(project_params))
+    write_file(profile_file_path,
+               apply_filter(template.render(project_params), filter_name))
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -48,6 +63,9 @@ if __name__ == '__main__':
 
     recipe = yaml.load(read_file(sys.argv[1]))
 
-    for project_name in recipe['projects']:
-        for template_name in recipe['templates']:
-            render_profile(project_name, template_name)
+    for filter_name in [None] + recipe['filters']:
+        for project_name in recipe['projects']:
+            for template_name in recipe['templates']:
+                render_profile(project_name,
+                               template_name,
+                               filter_name)
