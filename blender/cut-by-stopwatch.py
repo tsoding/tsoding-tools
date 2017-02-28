b609a0ba76e02f7d313b096464180abdd8cf9ce4
rexim <reximkut@gmail.com>
2017-02-28 20:27:13 +0700

    Reformat cut-by-stopwatch blender script

--- blender/cut-by-stopwatch.py
+++ blender/cut-by-stopwatch.py
@@ -1,20 +1,24 @@
 import csv
 import bpy
 
+
 fps = bpy.data.scenes['Scene'].render.fps / bpy.data.scenes['Scene'].render.fps_base
+
 
 def read_csv(csv_file_name):
     with open(csv_file_name, 'r') as csvfile:
         csvreader = csv.reader(csvfile)
         return [[comment, int(secs)] for [comment, secs] in csvreader]
 
+
 def cut_at(frame):
     bpy.ops.sequencer.select_all(action='SELECT')
     bpy.ops.sequencer.cut(frame=frame, type='SOFT')
 
+
 def cut_by_stopwatch(stopwatch_filepath):
     cut_at(0)
-    for [_, secs] in read_csv(stopwatch):
+    for [_, secs] in read_csv(stopwatch_filepath):
         cut_at(secs * fps)
 
 cut_by_stopwatch("/home/rexim/MorningTsoding/Morganey/Ep.27/1480862482sec")
