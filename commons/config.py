0082f11dd4d6516b0012b9bd26e6693df3d76c61
rexim <reximkut@gmail.com>
2017-05-27 18:47:20 +0700

    Rename get_tsoding_config -> home_tsoding_config (#14)

--- commons/config.py
+++ commons/config.py
@@ -2,5 +2,5 @@ from easyfile import read_yaml_file
 from os.path import expanduser
 
 
-def get_tsoding_config():
+def home_tsoding_config():
     return read_yaml_file(expanduser("~/.tsoding.yaml"))
