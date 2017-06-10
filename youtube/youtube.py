b3223c8183a3510e0b4ba0649a8e6023bb98ea30
rexim <reximkut@gmail.com>
2017-06-10 19:51:54 +0700

    Implement unlist_playlist_videos function (#17)

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -1,15 +1,21 @@
 import httplib2
-import os
-import re
 import sys
 
 from apiclient.discovery import build
-from apiclient.errors import HttpError
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run_flow
 
 from functional import arrow
+from functools import partial
+
+
+def clean_kwargs(**kwargs):
+    return {
+        key: value
+        for key, value in kwargs.iteritems()
+        if value is not None
+    }
 
 
 # TODO(#20): document how to retrieve the client secrets file
@@ -45,10 +51,60 @@ def ytservice_from_config(tsoding_config):
                  ytservice_from_credentials)
 
 
-# TODO(f2430622-3245-41e3-9761-35b65f40e9fa): implement unlist_playlist_videos()
-def unlist_playlist_videos(ytservice, playlist):
+def playlist_video_page(ytservice, playlist_id, page_token):
+    return ytservice.playlistItems().list(
+        **clean_kwargs(
+            part='snippet',
+            maxResults=50,
+            playlistId=playlist_id,
+            pageToken=page_token
+        )
+    ).execute()
+
+
+def collect_all_pages(next_page):
+    current_page = next_page(None)
+    yield current_page
+
+    while 'nextPageToken' in current_page:
+        current_page = next_page(current_page['nextPageToken'])
+        yield current_page
+
+
+def video_pages_as_id_list(video_pages):
+    return [item['snippet']['resourceId']['videoId']
+            for page in video_pages
+            for item in page['items']]
+
+
+def video_ids_of_playlist(ytservice, playlist_id):
+    return video_pages_as_id_list(
+        collect_all_pages(
+            partial(
+                playlist_video_page,
+                ytservice,
+                playlist_id
+            )
+        )
+    )
+
+
+def unlist_video(ytservice, video_id):
+    return ytservice.videos().update(
+        body={
+            'id': video_id,
+            'status': {
+                'privacyStatus': 'unlisted'
+            }
+        },
+        part='status'
+    ).execute()
+
+
+def unlist_playlist_videos(ytservice, playlist_id):
     """Unlists all of the videos in a specific playlist using YouTube API.
 
     Doesn't unlist the playlist itself. Only the videos.
     """
-    pass
+    return map(partial(unlist_video, ytservice),
+               video_ids_of_playlist(ytservice, playlist_id))
