f93febc387fc05171ef778c5586230b8620a2ac0
rexim <reximkut@gmail.com>
2018-05-30 17:42:14 +0700

    Document the Schedule import process

--- Schedule/Core.hs
+++ Schedule/Core.hs
@@ -1,3 +1,14 @@
+{-|
+- Use `generateCsvScheduleThisMonth` or `generateCsvScheduleNextMonth` to generate a CSV file.
+- Open Google Calendar on a computer. Note: You can only import from a computer, not a phone or tablet.
+- In the top right, click Settings
+- Open the Calendars tab.
+- Click Import calendar between the "My calendars" and "Other Calendars" sections.
+- Click Choose File and select the file you exported. The file should end in "ics" or "csv"
+- Choose which calendar to add the imported events to. By default, events will be imported into your primary calendar.
+- Click Import.
+- More info on https://support.google.com/calendar/answer/37118?hl=en
+-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 module Schedule.Core where
